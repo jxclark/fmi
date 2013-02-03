8b836dfdba059ab0f1f1d61585db0f34ee45d93a
jclarkCoder@gmail.com <>
2013-02-03 21:53:51 +0000

    Created an empty Album module.

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -1,42 +1,24 @@
 <?php
-  namespace Album;
-  
-  class Module
-  {
+namespace Album;
+
+class Module
+{
     public function getAutoloaderConfig()
     {
-      return array(
-          'Zend\Loader\ClassMapAutoloader' => array(
-            __DIR__ . 'autoload_classmap.php',
-      ),
-      'Zend\Loader\ClassMapAutoloader' => array(
-           'namespaces' => array(
-                __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
             ),
-          ),  
-    };
-  }
-        
-     public function getConfig()
-     {
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
         return include __DIR__ . '/config/module.config.php';
-     }    
+    }
 }
-      
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
